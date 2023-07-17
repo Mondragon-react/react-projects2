de648463dfae947325462d582b7017aa545d6669
daniel mondragon <daniel.mondragon004@GMAIL.COM>
2023-07-17 15:44:59 -0500

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,30 @@
 import React from "react" ;
+import {footer, blog, possibility, features, whatGPT3, header } from "./containers";
+import { cta, article, brand, navbar, feature} from "./components";
+// import WhatGPT3 from "./containers/whatGPT3/whatGPT3";
+// import Features from "./containers/features/features";
+// import Possibility from "./containers/possibility/possibility";
+// import CTA from "./components/cta/CTA";
+// import Blog from "./containers/blog/blog";
+// import Footer from "./containers/footer/footer";
+// import Navbar from "./components/navbar/Navbar";
+// import Header from "./containers/header/header";
+// import Brand from "./components/brand/Brand";
+
 const App = () => {
     return (
-        <div>
-            <h1>GPT-3</h1>
+        <div className="App">
+            <div className="gradient_bg">
+                <Navbar/>
+                <Header/>
+            </div>
+            <Brand/>
+            <WhatGPT3/>
+            <Features/>
+            <Possibility/>
+            <CTA/>
+            <Blog/>
+            <Footer/>
         </div>
     )
 }
